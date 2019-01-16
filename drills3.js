7e535c9143ac0533b79fcbbd82593ce1746f5892
Payman Kossari <Paymankossari@gmail.com>
2019-01-16 14:17:19 -0500

    iterating object properties drill added

--- drills3.js
+++ drills3.js
@@ -10,3 +10,17 @@ const loaf = {
 console.log(loaf.flour);
 console.log(loaf.water);
 console.log(loaf.hydration());
+
+//drill 2
+
+let obj = {
+  foo: 60,
+  bar: 'hi',
+  fum: 'hello',
+  quux: 200,
+  spam: null
+};
+
+for (let key in obj) {
+  console.log(`${key}: ${obj[key]}`);
+}
